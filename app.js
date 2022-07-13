1062bc3d17875c5e03fb9ee8d5c508dccae95a11
ArieL <alcarazsincuenta@gmail.com>
2022-07-13 19:39:45 -0300

    update

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.static('public'))
+
+app.listen(3000, () => {
+    console.log('Servidor corriendo')
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/index.html'))
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'))
+})
